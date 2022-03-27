62260b83cdd8bfd871d1c538626d6936c0930978
sshinohs <sshinohs@gmail.com>
2022-03-27 21:41:49 +0900

    Post 파일 생성, 리디렉션

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+var qs = require('querystring');
 
 function templateHTML(title, list, body) {
   return `
@@ -74,6 +75,20 @@ var app = http.createServer(function(request, response) {
         `);
       response.writeHead(200);
       response.end(template);
+    });
+  } else if(pathname === '/create_process') {
+    var body = '';
+    request.on('data', function(data) {
+      body = body + data;
+    });
+    request.on('end', function() {
+      var post = qs.parse(body);
+      var title = post.title;
+      var description = post.description;
+      fs.writeFile(`data/${title}`, description, 'utf8', function(err) {
+        response.writeHead(302, {Location: `/?id=${title}`});
+        response.end();
+      })
     });
   } else {
     response.writeHead(404);
